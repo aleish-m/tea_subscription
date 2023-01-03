e8aa92324006df67aad4499cb0a2bf36e2aa7171
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-03 14:53:51 -0700

    Feat: Add tea model and migration

---
+++ db/migrate/20230103211836_create_teas.rb
@@ -0,0 +1,13 @@
+class CreateTeas < ActiveRecord::Migration[5.2]
+  def change
+    create_table :teas do |t|
+      t.string :title
+      t.string :description
+      t.integer :temperature
+      t.integer :brew_time
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
