f77b0605e8cd1ea1d66db771b217dbdcfcd251fb
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-03 14:59:42 -0700

    Test/Feat: Add model validations for tea

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe Tea, type: :model do
     it{should have_many(:tea_subscriptions)}
     it{should have_many(:subscriptions).through(:tea_subscriptions)}
   end
+
+  describe 'validations' do 
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :temperature }
+    it { should validate_presence_of :brew_time }
+    it { should validate_presence_of :price }
+  end
 end
 
