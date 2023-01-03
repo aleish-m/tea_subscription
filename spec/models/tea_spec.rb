4ec0326b29f212e382a5060ed8e9f5e182d87d93
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-03 15:18:58 -0700

    Teast: Add test for user model relationship and validations and add factory

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
   describe 'relationships' do
-    it{should have_many(:tea_subscriptions)}
-    it{should have_many(:subscriptions).through(:tea_subscriptions)}
+    it { should have_many(:tea_subscriptions) }
+    it { should have_many(:subscriptions).through(:tea_subscriptions) }
   end
 
   describe 'validations' do 
