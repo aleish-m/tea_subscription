98170d24e25295000dfa1d409222e5dec4530b8e
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-04 13:53:26 -0700

    Feat: add a method to calculat cost of all teas in a subscription

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -3,4 +3,8 @@ class Tea < ApplicationRecord
   has_many :subscriptions, through: :teas_subscriptions
 
   validates_presence_of :title, :description, :temperature, :brew_time, :price
+
+  def self.total_cost
+    sum(:price)
+  end
 end
