be43b862f22c19666178310daf3922860304ca94
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-03 18:16:39 -0700

    Feat: Add relationship to subscriptions to user

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,7 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :user
+
   enum status: {active: 0, canceled: 1}
+  
   validates_presence_of :title, :status, :frequency, :user_id
   validates_numericality_of :frequency
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :subscriptions
+  
   validates_presence_of :first_name, :last_name, :email, :address
   validates_uniqueness_of :email
 end
