2a1d02c606b98f9b7cb5417a43203146e4cbff46
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-04 13:17:29 -0700

    Add create subscription route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+        resources :subscription, only: [:create]
+      end
+    end
+  end
 end
