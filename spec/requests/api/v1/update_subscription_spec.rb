d2ea8dbd4cd7f024aeaa9d300e49f537a27fc6d7
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-05 10:16:25 -0700

    Correct spec variable nameing due to conflicts

--- spec/requests/api/v1/update_subscription_spec.rb
+++ spec/requests/api/v1/update_subscription_spec.rb
@@ -10,8 +10,8 @@ describe 'Update Subscriptions' do
 
       patch "/api/v1/users/#{user.id}/subscription/#{subscription.id}", params: params
 
-      subscription_data = JSON.parse(response.body, symbolize_names: true)
-      subscription = subscriptions_data[:data]
+      subscription_response = JSON.parse(response.body, symbolize_names: true)
+      subscription_data = subscription_response[:data]
 
       updated_subscription = Subscription.find(subscription.id)
 
@@ -23,17 +23,17 @@ describe 'Update Subscriptions' do
       expect(updated_subscription.frequency).to eq(subscription.frequency)
       expect(updated_subscription.id).to eq(subscription.id)
 
-      expect(subscription_data).to be_a(Hash)
-      expect(subscription_data.count).to eq(1)
+      expect(subscription_response).to be_a(Hash)
+      expect(subscription_response.count).to eq(1)
 
-      expect(subscription[:id].to_i).to eq(subscription.id)
-      expect(subscription[:type]).to eq('subscription')
-      expect(subscription[:attributes].count).to eq(5)
-      expect(subscription[:attributes][:title]).to eq('New Subscription')
-      expect(subscription[:attributes][:status]).to eq('cancelled')
-      expect(subscription[:attributes][:frequency]).to eq(subscription.frequency)
-      expect(subscription[:attributes][:price]).to eq(Tea.total_cost)
-      expect(subscription[:attributes][:created_at].to_date).to eq(created_subscription.created_at.to_date)
+      expect(subscription_data[:id].to_i).to eq(subscription.id)
+      expect(subscription_data[:type]).to eq('subscription')
+      expect(subscription_data[:attributes].count).to eq(5)
+      expect(subscription_data[:attributes][:title]).to eq(subscription.title)
+      expect(subscription_data[:attributes][:status]).to eq('cancelled')
+      expect(subscription_data[:attributes][:frequency]).to eq(subscription.frequency)
+      expect(subscription_data[:attributes][:price]).to eq(Tea.total_cost)
+      expect(subscription_data[:attributes][:created_at].to_date).to eq(subscription.created_at.to_date)
 
     end
   end
