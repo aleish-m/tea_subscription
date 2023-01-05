4557c076ac0573e3246006a13a4e38364fb03b68
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-04 22:08:21 -0700

    Addt route to view a users subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index] do
-        resources :subscription, only: [:create]
+        resources :subscription, only: [:create, :index]
       end
     end
   end
